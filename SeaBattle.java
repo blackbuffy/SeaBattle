142eef7e0b6bf4162c1a4ea1c735ded72f805e01
buffy <132291503+blackbuffy@users.noreply.github.com>
2023-06-07 09:38:33 +0300

    Update SeaBattle.java

--- SeaBattle.java
+++ SeaBattle.java
@@ -4,6 +4,33 @@ public class SeaBattle {
     public static void main(String[] args) {
         // Field initializing
         Scanner sc = new Scanner(System.in);
+        String[][] fogField = new String[11][11];
+        fogField[0][0] = " ";
+        fogField[0][1] = "1";
+        fogField[0][2] = "2";
+        fogField[0][3] = "3";
+        fogField[0][4] = "4";
+        fogField[0][5] = "5";
+        fogField[0][6] = "6";
+        fogField[0][7] = "7";
+        fogField[0][8] = "8";
+        fogField[0][9] = "9";
+        fogField[0][10] = "10";
+        fogField[1][0] = "A";
+        fogField[2][0] = "B";
+        fogField[3][0] = "C";
+        fogField[4][0] = "D";
+        fogField[5][0] = "E";
+        fogField[6][0] = "F";
+        fogField[7][0] = "G";
+        fogField[8][0] = "H";
+        fogField[9][0] = "I";
+        fogField[10][0] = "J";
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                fogField[i][j] = "~";
+            }
+        }
         String[][] field = new String[11][11];
         field[0][0] = " ";
         field[0][1] = "1";
@@ -31,14 +58,7 @@ public class SeaBattle {
                 field[i][j] = "~";
             }
         }
-        // Printing field
-        for (int k = 0; k <= 10; k++) {
-            for (int x = 0; x <= 10; x++) {
-                System.out.print(field[k][x]);
-                System.out.print(" ");
-            }
-            System.out.println("");
-        }
+        printField(field);
         // Asking for AC coord and setting ship
         System.out.printf("Enter the coordinates of the %s (%d cells): ", Ships.AC.label, Ships.AC.cells);
         String coordAC = sc.nextLine();
@@ -49,13 +69,7 @@ public class SeaBattle {
             AC = new Ship(coordAC, "Aircraft Carrier", 5, field);
             setAC = setShip(AC, field, AC.getRowStart(), AC.getColumnStart(), AC.getRowEnd(), AC.getColumnEnd());
         }
-        for (int k = 0; k <= 10; k++) {
-            for (int x = 0; x <= 10; x++) {
-                System.out.print(field[k][x]);
-                System.out.print(" ");
-            }
-            System.out.println("");
-        }
+        printField(field);
         // Asking for BS coord and setting ship
         System.out.printf("Enter the coordinates of the %s (%d cells): ", Ships.BS.label, Ships.BS.cells);
         String coordBS = sc.nextLine();
@@ -66,13 +80,7 @@ public class SeaBattle {
             BS = new Ship(coordBS, Ships.BS.label, Ships.BS.cells, field);
             setBS = setShip(BS, field, BS.getRowStart(), BS.getColumnStart(), BS.getRowEnd(), BS.getColumnEnd());
         }
-        for (int k = 0; k <= 10; k++) {
-            for (int x = 0; x <= 10; x++) {
-                System.out.print(field[k][x]);
-                System.out.print(" ");
-            }
-            System.out.println("");
-        }
+        printField(field);
         // Asking for SM coord and setting ship
         System.out.printf("Enter the coordinates of the %s (%d cells): ", Ships.SM.label, Ships.SM.cells);
         String coordSM = sc.nextLine();
@@ -83,13 +91,7 @@ public class SeaBattle {
             SM = new Ship(coordSM, Ships.SM.label, Ships.SM.cells, field);
             setSM = setShip(SM, field, SM.getRowStart(), SM.getColumnStart(), SM.getRowEnd(), SM.getColumnEnd());
         }
-        for (int k = 0; k <= 10; k++) {
-            for (int x = 0; x <= 10; x++) {
-                System.out.print(field[k][x]);
-                System.out.print(" ");
-            }
-            System.out.println("");
-        }
+        printField(field);
         // Asking for CR coord and setting ship
         System.out.printf("Enter the coordinates of the %s (%d cells): ", Ships.CR.label, Ships.CR.cells);
         String coordCR = sc.nextLine();
@@ -100,13 +102,7 @@ public class SeaBattle {
             CR = new Ship(coordCR, Ships.CR.label, Ships.CR.cells, field);
             setCR = setShip(CR, field, CR.getRowStart(), CR.getColumnStart(), CR.getRowEnd(), CR.getColumnEnd());
         }
-        for (int k = 0; k <= 10; k++) {
-            for (int x = 0; x <= 10; x++) {
-                System.out.print(field[k][x]);
-                System.out.print(" ");
-            }
-            System.out.println("");
-        }
+        printField(field);
         // Asking for DS coord and setting ship
         System.out.printf("Enter the coordinates of the %s (%d cells): ", Ships.DS.label, Ships.DS.cells);
         String coordDS = sc.nextLine();
@@ -117,12 +113,23 @@ public class SeaBattle {
             DS = new Ship(coordDS, Ships.DS.label, Ships.DS.cells, field);
             setDS = setShip(DS, field, DS.getRowStart(), DS.getColumnStart(), DS.getRowEnd(), DS.getColumnEnd());
         }
-        for (int k = 0; k <= 10; k++) {
-            for (int x = 0; x <= 10; x++) {
-                System.out.print(field[k][x]);
-                System.out.print(" ");
+        printField(field);
+        // First shot
+        System.out.println("The game starts!");
+        printField(fogField);
+        System.out.println("Take a shot!");
+        String shotCoord = sc.nextLine();
+        int shot = shoot(shotCoord, fogField, field);
+        infLoop:
+        while (calcO(field) > 0) {
+            shotCoord = sc.nextLine();
+            shot = shoot(shotCoord, fogField, field);
+            if (shot != 0) {
+                while (shot != 0) {
+                    shotCoord = sc.nextLine();
+                    shot = shoot(shotCoord, fogField, field);
+                }
             }
-            System.out.println("");
         }
     }
 
@@ -214,6 +221,98 @@ public class SeaBattle {
             for (int i = Math.min(rowStart, rowEnd); i <= Math.max(rowStart, rowEnd); i++) {
                 field[i][columnStart] = "O";
             }
+        }
+        return ret;
+    }
+
+    public static void printField(String[][] field) {
+        for (int k = 0; k <= 10; k++) {
+            for (int x = 0; x <= 10; x++) {
+                System.out.print(field[k][x]);
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static int calcO(String[][] field) {
+        int O = 0;
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j].equals("O")) {
+                    O++;
+                }
+            }
+        }
+        return O;
+    }
+
+    public static int shoot(String coord, String[][] fogField, String[][] field) {
+        int ret = 0;
+        int row = 0;
+        for (int i = 0; i < field.length; i++) {
+            if (field[i][0].equals(String.valueOf(coord.charAt(0)))) {
+                row = i;
+            }
+        }
+        if (row == 0) {
+            System.out.println("Error! You entered the wrong coordinates! Try again: ");
+            ret = 1;
+        }
+
+        int column = 0;
+        for (int i = 0; i < field[0].length; i++) {
+            if (coord.length() == 3) {
+                if (Integer.parseInt(String.valueOf(coord.charAt(2))) == 0) {
+                    column = 10;
+                    break;
+                } else {
+                    System.out.println("Error! You entered the wrong coordinates! Try again: ");
+                    ret = 1;
+                    break;
+                }
+            } else if (field[0][i].equals(String.valueOf(coord.charAt(1)))) {
+                column = i;
+            }
+        }
+        if (column == 0 && row != 0) {
+            System.out.println("Error! You entered the wrong coordinates! Try again: ");
+            ret = 1;
+        }
+
+        int O = 0;
+        if (field[row][column].equals("O") && ret != 1) {
+            fogField[row][column] = "X";
+            field[row][column] = "X";
+            outerLoop:
+            for (int i = Math.max(0, row - 1); i <= Math.min(10, row + 1); i++) {
+                for (int j = Math.max(0, column - 1); j <= Math.min(10, column + 1); j++) {
+                    if (field[i][j].equals("O")) {
+                        O++;
+                    }
+                }
+            }
+            if (calcO(field) == 0) {
+                printField(fogField);
+                System.out.println("You sank the last ship. You won. Congratulations!");
+            } else if (O > 0) {
+                printField(fogField);
+                System.out.println("You hit a ship! Try again:");
+            } else if (O == 0) {
+                printField(fogField);
+                System.out.println("You sank a ship! Specify a new target:");
+            }
+        } else if (field[row][column].equals("~") && ret != 1) {
+            fogField[row][column] = "M";
+            field[row][column] = "M";
+            printField(fogField);
+            System.out.println("You missed! Try again:");
+        } else if (field[row][column].equals("X") && ret != 1) {
+            printField(fogField);
+            System.out.println("You hit a ship! Try again:");
+        } else if (field[row][column].equals("M") && ret != 1) {
+            printField(fogField);
+            System.out.println("You missed! Try again:");
         }
         return ret;
     }
